b192cb609ea6779d54c4d65f6b2f3d44419e610b
Jacob Liddy <jpl61@zips.uakron.edu>
2017-04-14 04:41:03 +0000

    Adding burst.c

---
+++ burst.c
@@ -0,0 +1,25 @@
+/*
+  Project: Burst
+
+  Contributers:
+  Jacob Liddy
+  mlcollard
+  av95
+
+  Description: 
+    Split a file into several smaller files, process them individually
+*/
+
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+    /* First do error handling, input processing*/
+
+
+    /* Then create files, and use fork to split into different processes */
+
+    fork();
+
+
+    return 0;
+}
